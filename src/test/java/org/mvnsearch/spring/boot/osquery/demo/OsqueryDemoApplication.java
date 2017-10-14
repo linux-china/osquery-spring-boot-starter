e18e2701cdf5886d85c95e0f28bdf62a9aaf18b6
linux_china <linux_china@hotmail.com>
2017-10-14 10:51:47 +0800

    osquery demo application

---
+++ src/test/java/org/mvnsearch/spring/boot/osquery/demo/OsqueryDemoApplication.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.spring.boot.osquery.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * osquery demo application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class OsqueryDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OsqueryDemoApplication.class, args);
+    }
+}

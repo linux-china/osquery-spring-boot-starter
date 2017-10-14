ebd3ddc8805fc04ce1ba3cc0751aedca4fd340fb
linux_china <linux_china@hotmail.com>
2017-10-14 10:50:14 +0800

    process result

---
+++ src/main/java/org/mvnsearch/osquery/jdbc/ProcessResult.java
@@ -0,0 +1,42 @@
+package org.mvnsearch.osquery.jdbc;
+
+/**
+ * process result
+ *
+ * @author linux_china
+ */
+public class ProcessResult {
+    private int exitValue;
+    private String output;
+    private String error;
+
+    public ProcessResult(int exitValue, String output, String error) {
+        this.exitValue = exitValue;
+        this.output = output;
+        this.error = error;
+    }
+
+    public int getExitValue() {
+        return exitValue;
+    }
+
+    public void setExitValue(int exitValue) {
+        this.exitValue = exitValue;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}

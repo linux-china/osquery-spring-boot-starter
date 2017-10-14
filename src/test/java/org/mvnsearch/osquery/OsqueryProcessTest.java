616dea8ec4e90b0b94bd73134fb8c414ff88a0b7
linux_china <linux_china@hotmail.com>
2017-10-14 10:52:22 +0800

    osquery process test

---
+++ src/test/java/org/mvnsearch/osquery/OsqueryProcessTest.java
@@ -0,0 +1,32 @@
+package org.mvnsearch.osquery;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * osquery process test
+ *
+ * @author linux_china
+ */
+public class OsqueryProcessTest {
+    private OsqueryProcess osquery = new OsqueryProcess();
+
+    @Test
+    public void testGetVersion() {
+        System.out.println(osquery.getVersion());
+    }
+
+    @Test
+    public void testGetTables() {
+        List<String> tables = osquery.getTables();
+        for (String table : tables) {
+            System.out.println(table.trim());
+        }
+    }
+
+    @Test
+    public void testGetTableContent() {
+        System.out.println(osquery.getTableOutput("docker_images", "json").getOutput());
+    }
+}

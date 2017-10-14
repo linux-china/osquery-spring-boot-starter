0614059ab6777b1866d9724995bef1fad8eee031
linux_china <linux_china@hotmail.com>
2017-10-14 10:48:01 +0800

    osquery process

---
+++ src/main/java/org/mvnsearch/osquery/OsqueryProcess.java
@@ -0,0 +1,68 @@
+package org.mvnsearch.osquery;
+
+import org.mvnsearch.osquery.jdbc.ProcessResult;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * osquery process
+ *
+ * @author linux_china
+ */
+public class OsqueryProcess {
+
+    public String getVersion() {
+        ProcessResult result = execute("--version");
+        return result.getOutput().substring(result.getOutput().lastIndexOf(" ") + 1).trim();
+    }
+
+    public List<String> getTables() {
+        ProcessResult result = execute("--L");
+        List<String> tables = new ArrayList<>();
+        String[] lines = result.getOutput().split(System.lineSeparator());
+        for (String line : lines) {
+            tables.add(line.replace("=>", "").trim());
+        }
+        return tables;
+    }
+
+    public ProcessResult getTableOutput(String table, String format) {
+        if (StringUtils.isEmpty(format)) {
+            return execute("--A", table);
+        } else {
+            return execute("--A", table, "--" + format);
+        }
+    }
+
+    public ProcessResult query(String sql, String format) {
+        if (StringUtils.isEmpty(format)) {
+            return execute(sql);
+        } else {
+            return execute("--" + format, sql);
+        }
+    }
+
+    private ProcessResult execute(String... args) {
+        try {
+            List<String> command = new ArrayList<>();
+            command.add("osqueryi");
+            command.addAll(Arrays.asList(args));
+            ProcessBuilder pb = new ProcessBuilder(command);
+            Process p = pb.start();
+            String output = StreamUtils.copyToString(p.getInputStream(), Charset.defaultCharset());
+            String error = StreamUtils.copyToString(p.getErrorStream(), Charset.defaultCharset());
+            //Wait to get exit value
+            int exitValue = p.waitFor();
+            return new ProcessResult(exitValue, output, error);
+        } catch (Exception e) {
+            return new ProcessResult(-1, null, e.getMessage());
+        }
+    }
+
+
+}

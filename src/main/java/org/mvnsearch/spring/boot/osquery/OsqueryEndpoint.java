e12b344a485bac0a79f5f785277b173b5e718f14
linux_china <libing.chen@gmail.com>
2019-10-04 17:47:13 -0700

    add osquery by column

--- src/main/java/org/mvnsearch/spring/boot/osquery/OsqueryEndpoint.java
+++ src/main/java/org/mvnsearch/spring/boot/osquery/OsqueryEndpoint.java
@@ -2,9 +2,9 @@ package org.mvnsearch.spring.boot.osquery;
 
 import org.mvnsearch.osquery.OsqueryProcess;
 import org.mvnsearch.osquery.jdbc.ProcessResult;
-import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
-import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
-import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -15,7 +15,7 @@ import java.util.Map;
  *
  * @author linux_china
  */
-@Endpoint(id = "osquery", enableByDefault = true)
+@RestControllerEndpoint(id = "osquery", enableByDefault = true)
 public class OsqueryEndpoint {
     private OsqueryProcess osquery;
 
@@ -23,7 +23,7 @@ public class OsqueryEndpoint {
         osquery = new OsqueryProcess();
     }
 
-    @ReadOperation
+    @GetMapping
     public Map<String, Object> invoke() {
         Map<String, Object> info = new HashMap<>();
         String version = osquery.getVersion();
@@ -34,9 +34,16 @@ public class OsqueryEndpoint {
         return info;
     }
 
-    @ReadOperation
-    public String tableShow(@Selector String tableName) throws IOException {
+    @GetMapping("/{tableName}")
+    public String tableShow(@PathVariable(name = "tableName") String tableName) throws IOException {
         ProcessResult result = osquery.getTableOutput(tableName, "json");
+        return result.getOutput();
+    }
+
+    @GetMapping("/{tableName}/{columnNames}")
+    public String tableShow(@PathVariable(name = "tableName") String tableName,
+                            @PathVariable(name = "columnNames") String columnNames) throws IOException {
+        ProcessResult result = osquery.query("select " + columnNames + " from " + tableName, "json");
         return result.getOutput();
     }
 

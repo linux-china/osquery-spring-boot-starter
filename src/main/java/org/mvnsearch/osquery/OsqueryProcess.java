ef2e02ef3028649af348296ead7b06463d8b4cd9
Libing Chen <libing.chen@gmail.com>
2024-12-12 18:17:39 +0800

    chore: add csv support

--- src/main/java/org/mvnsearch/osquery/OsqueryProcess.java
+++ src/main/java/org/mvnsearch/osquery/OsqueryProcess.java
@@ -16,53 +16,55 @@ import java.util.List;
  */
 public class OsqueryProcess {
 
-    public String getVersion() {
-        ProcessResult result = execute("--version");
-        return result.getOutput().substring(result.getOutput().lastIndexOf(" ") + 1).trim();
-    }
+  public String getVersion() {
+    ProcessResult result = execute("--version");
+    return result.getOutput().substring(result.getOutput().lastIndexOf(" ") + 1).trim();
+  }
 
-    public List<String> getTables() {
-        ProcessResult result = execute("--L");
-        List<String> tables = new ArrayList<>();
-        String[] lines = result.getOutput().split(System.lineSeparator());
-        for (String line : lines) {
-            tables.add(line.replace("=>", "").trim());
-        }
-        return tables;
+  public List<String> getTables() {
+    ProcessResult result = execute("--L");
+    List<String> tables = new ArrayList<>();
+    String[] lines = result.getOutput().split(System.lineSeparator());
+    for (String line : lines) {
+      tables.add(line.replace("=>", "").trim());
     }
+    return tables;
+  }
 
-    public ProcessResult getTableOutput(String table, String format) {
-        if (StringUtils.isEmpty(format)) {
-            return execute("--A", table);
-        } else {
-            return execute("--A", table, "--" + format);
-        }
+  public ProcessResult getTableOutput(String table, String format) {
+    if (StringUtils.isEmpty(format)) {
+      return execute("--A", table);
+    } else if (format.equals("csv")) {
+      return execute("--csv", "--separator", ",", "--A", table);
+    } else {
+      return execute("--A", table, "--" + format);
     }
+  }
 
-    public ProcessResult query(String sql, String format) {
-        if (StringUtils.isEmpty(format)) {
-            return execute(sql);
-        } else {
-            return execute("--" + format, sql);
-        }
+  public ProcessResult query(String sql, String format) {
+    if (StringUtils.isEmpty(format)) {
+      return execute(sql);
+    } else {
+      return execute("--" + format, sql);
     }
+  }
 
-    private ProcessResult execute(String... args) {
-        try {
-            List<String> command = new ArrayList<>();
-            command.add("osqueryi");
-            command.addAll(Arrays.asList(args));
-            ProcessBuilder pb = new ProcessBuilder(command);
-            Process p = pb.start();
-            String output = StreamUtils.copyToString(p.getInputStream(), Charset.defaultCharset());
-            String error = StreamUtils.copyToString(p.getErrorStream(), Charset.defaultCharset());
-            //Wait to get exit value
-            int exitValue = p.waitFor();
-            return new ProcessResult(exitValue, output, error);
-        } catch (Exception e) {
-            return new ProcessResult(-1, null, e.getMessage());
-        }
+  private ProcessResult execute(String... args) {
+    try {
+      List<String> command = new ArrayList<>();
+      command.add("osqueryi");
+      command.addAll(Arrays.asList(args));
+      ProcessBuilder pb = new ProcessBuilder(command);
+      Process p = pb.start();
+      String output = StreamUtils.copyToString(p.getInputStream(), Charset.defaultCharset());
+      String error = StreamUtils.copyToString(p.getErrorStream(), Charset.defaultCharset());
+      //Wait to get exit value
+      int exitValue = p.waitFor();
+      return new ProcessResult(exitValue, output, error);
+    } catch (Exception e) {
+      return new ProcessResult(-1, null, e.getMessage());
     }
+  }
 
 
 }
